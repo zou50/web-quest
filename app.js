035951d3c50cc57c725b36ad3a698c5a5f648951
Danny Zou <zou50@purdue.edu>
2017-12-02 22:23:11 -0500

    Initialize server code and scripts

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-// primary app entry point
+// Primary server entry point
+
+// server setup
+
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io').listen(server);
+
+// path handling
+
+app.use('/js', express.static(__dirname + '/js'));
+app.use('/assets', express.static(__dirname + '/assets'));
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+server.listen(process.env.PORT || 3000, function() {
+    console.log("Server started on port " + server.address().port);
+});
+
