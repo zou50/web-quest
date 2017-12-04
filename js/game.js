4fc4e19f4aaac398373505a15e7e8f27fbce9210
juddmic <mjudd@purdue.edu>
2017-12-04 04:28:34 -0500

    map spritesheet

--- js/game.js
+++ js/game.js
@@ -33,10 +33,10 @@ Game.create = function() {
 	
 	//Spritesheet example on adding a sprite, 	0-624, 636 = looped
 
-	npc1 = game.add.sprite(0,0,'characters', sprites["white"]);
-	npc2 = game.add.sprite(0,16,'characters', sprites["white_open"]);
-	weapon = game.add.sprite(0,32,'characters', sprites["BLAAAACK"]);
-    game.add.sprite(0, 48, 'characters', 537);
+	npc1 = game.add.sprite(0,0,'characters', sprites["white_male"]);
+	npc2 = game.add.sprite(0,16,'characters', sprites["tan_male"]);
+	weapon = game.add.sprite(0,32,'characters', sprites["black_male"]);
+    game.add.sprite(0, 48, 'characters', sprites["shortbow"]);
 	
     // player
     player = game.add.sprite(randomInt(0, 200), randomInt(0, 200), 'player');
